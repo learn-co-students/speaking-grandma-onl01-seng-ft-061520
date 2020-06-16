ad8367bef0912458dc5dcf396403429277f9f3af
beautiful-instructions-5625 <slen.detablan@gmail.com>
2020-06-16 17:11:47 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ if phrase == 'I LOVE YOU GRANDMA!'
 end
 
 if phrase != phrase.upcase()
-  return "HUH?! SPEAK UP SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 else
     return "NO, NOT SINCE 1938!"
   end
