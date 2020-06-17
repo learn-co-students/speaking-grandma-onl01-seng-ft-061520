7f5064b8737f5790151598afad71db0542239d28
kidtyler <tyleremanuel88@gmail.com>
2020-06-17 18:27:19 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,10 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+    phrase="hey granny"
+   if phrase= phrase.upcase
+ puts "HUH?! SPEAK UP, SONNY!."
+   end
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
